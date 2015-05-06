225ff05ae339ea143561d4187ef9dc00679c961a
fuchida <abdlsalim@gmail.com>
2015-05-05 21:02:14 -0500

    Add filtering of 200 responses

--- src/vamp.py
+++ src/vamp.py
@@ -1,6 +1,8 @@
 import requests
 from lxml import html
 
+# TODO: Detect relative urls on a page
+
 
 class Vamp(object):
 
@@ -20,9 +22,9 @@ class Vamp(object):
         """
         response = requests.get(self.url)
         page_urls = self.get_page_urls(response.text)
-        urls_and_statuses = self.chech_url_status(page_urls)
-
-        return urls_and_statuses
+        urls_and_statuses = self._check_url_status(page_urls)
+        dead_links = self._filter_ok_responses(urls_and_statuses)
+        return dead_links
 
     def get_page_urls(self, html_page):
         """
@@ -39,7 +41,7 @@ class Vamp(object):
         links = tree.xpath('//a/@href')
         return(links)
 
-    def chech_url_status(self, urls):
+    def _check_url_status(self, urls):
         """
         Given a list of URLs perform get requests and check http
         status of each URL
@@ -71,3 +73,12 @@ class Vamp(object):
                 A list of only urls that start with http
         """
         return [item for item in urls if item.startswith('http')]
+
+    def _filter_ok_responses(self, urls_and_statuses):
+        """
+        Go through a dictionary of urls and http statuses and
+        filter out 200 responses
+        """
+        return {url: status_code for
+                url, status_code in urls_and_statuses.items()
+                if status_code is not 200}
